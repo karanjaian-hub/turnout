6862d829761876ff1f661165a0f00e2ba917e239
karanjaian-hub <karanjaian420@gmail.com>
2026-06-13 20:46:38 +0300

    fix: rsvpservice credentials and schema alignment

--- services/rsvpservice/src/main/java/com/turnout/rsvpservice/config/AppConfig.java
+++ services/rsvpservice/src/main/java/com/turnout/rsvpservice/config/AppConfig.java
@@ -7,8 +7,6 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
-    // RestTemplate is in spring-web (already a transitive dep of spring-boot-starter-web).
-    // We register it as a bean so Spring can inject it into RsvpService.
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
